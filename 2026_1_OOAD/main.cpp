1a0d807a551090a5ea5135de15ce1f2753a6d152
davidpro08 <davidmoon0817@gmail.com>
2026-03-16 09:39:13 +0900

    test: add SampleAdd test case and refactor project structure

---
+++ 2026_1_OOAD/main.cpp
@@ -0,0 +1,8 @@
+#include "2026_1_OOAD.h"
+
+int main()
+{
+    std::cout << "Hello CMake." << std::endl;
+    std::cout << "3 + 5 = " << SampleAdd(3, 5) << std::endl;
+    return 0;
+}
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
+#include "../2026_1_OOAD/2026_1_OOAD.h"
 
 TEST(SampleTest, TrueIsTrue) {
   EXPECT_TRUE(true);
+}
+
+TEST(MathTest, SampleAddWorks) {
+  EXPECT_EQ(SampleAdd(1, 2), 3);
+  EXPECT_EQ(SampleAdd(-1, 1), 0);
+  EXPECT_EQ(SampleAdd(0, 0), 0);
 }
 
 int main(int argc, char **argv) {

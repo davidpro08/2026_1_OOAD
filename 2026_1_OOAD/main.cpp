70f12552feeeec7d7485f526b743d3baf64afc54
davidpro08 <davidmoon0817@gmail.com>
2026-03-16 09:51:53 +0900

    chore: insert github templete and sonarqube

--- 2026_1_OOAD/main.cpp
+++ 2026_1_OOAD/main.cpp
@@ -1,8 +1,12 @@
 #include "2026_1_OOAD.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello CMake." << std::endl;
-    std::cout << "3 + 5 = " << SampleAdd(3, 5) << std::endl;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "Hello CMake." << endl;
+    cout << "3 + 5 = " << SampleAdd(3, 5) << endl;
     return 0;
 }

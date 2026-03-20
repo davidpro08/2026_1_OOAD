fba29dfde25defc6f0df557636d2706319ff4bbe
davidpro08 <davidmoon0817@gmail.com>
2026-03-20 11:02:59 +0900

    chore: 코드 분석 및 테스트 수행

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,17 +1,12 @@
+#include "2026_1_OOAD.h" // 상대 경로 대신 CMake include path 활용
 #include <gtest/gtest.h>
-#include "../2026_1_OOAD/2026_1_OOAD.h"
 
 TEST(SampleTest, TrueIsTrue) {
-  EXPECT_TRUE(true);
+    EXPECT_TRUE(true);
 }
 
 TEST(MathTest, SampleAddWorks) {
-  EXPECT_EQ(SampleAdd(1, 2), 3);
-  EXPECT_EQ(SampleAdd(-1, 1), 0);
-  EXPECT_EQ(SampleAdd(0, 0), 0);
+    EXPECT_EQ(SampleAdd(1, 2), 3);
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+// main 함수는 CMake에서 GTest::gtest_main을 링크하면 자동으로 생성되므로 삭제 가능합니다.

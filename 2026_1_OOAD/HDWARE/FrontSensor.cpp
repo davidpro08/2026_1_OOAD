5e32e38383067d6871d7c10ef10ab10bb9376916
reload02 <63439524+reload02@users.noreply.github.com>
2026-05-05 04:07:58 +0900

    feat: implement ISensor interface and add sensor classes for dust, front, left, and right sensors

---
+++ 2026_1_OOAD/HDWARE/FrontSensor.cpp
@@ -0,0 +1,19 @@
+#include "IsenSor.h"
+
+class FrontSensor : public ISensor {
+private:
+    float detectedValue=-1.0;
+public:    
+    bool detect() override {
+        if (detectedValue > 0 && detectedValue < 5) {
+            return true;
+        }
+        return false;
+    }
+    void setFrontSensorValue(float value) {
+        detectedValue = value;
+    }//이거랑  detect를 메인이 틱마다 부르면 될듯?
+    //아니면 다르게 구현하던가 해야할듯 인터럽트라서
+    
+
+};

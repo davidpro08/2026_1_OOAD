75cbf138c9c6eb778cf3c8e0ca10a1087c1d37e9
davidpro08 <davidmoon0817@gmail.com>
2026-05-05 17:08:13 +0900

    test: 1차 테스트

--- tests/test_motor_controller.cpp
+++ tests/test_motor_controller.cpp
@@ -4,88 +4,150 @@
 #include "Class/SensorProvider.h"
 
 #include <gtest/gtest.h>
+#include <memory>
 
-namespace {
+namespace MotorControllerTest {
 
-class FakeSensorProvider : public SensorProvider {
-public:
-    bool left = true;
-    bool right = true;
+    class FakeSensorProvider : public SensorProvider {
+    public:
+        bool left = true;
+        bool right = true;
 
-    bool getLeftState() override { return left; }
-    bool getRightState() override { return right; }
-};
+        bool getLeftState() override { return left; }
+        bool getRightState() override { return right; }
+    };
 
-}  // namespace
+    class Point {
+        public:
+        int x;
+        int y;
+		Point(int x, int y) : x(x), y(y) {}
+        bool isEqual(Point other) {
+            return this->x == other.x && this->y == other.y;
+		}
+    };
 
-TEST(MotorController, TurnOnTurnOffDoesNotThrow) {
-    EventBus bus;
-    MotorController motor(&bus);
-    motor.turnOn();
-    motor.turnOff();
-}
+    class FakeMotor : public Motor {
 
-TEST(MotorController, PublishMoveForwardNotifiesSubscribers) {
-    EventBus bus;
-    MotorController motor(&bus);
-    int move_forward_count = 0;
-    bus.subScribeMoveForward([&move_forward_count] { ++move_forward_count; });
+    public:
+		Point point = Point(0, 0);
+		// 초기 방향은 위쪽으로 가도록 설정
+		Point direction = Point(0, 1);
+        void stop() {  }
+        void moveForward() { 
+            point.x = point.x + direction.x; 
+			point.y = point.y + direction.y;
+        }
+        void turnLeft() {
+			Point temp = direction;
+			direction.x = -temp.y;
+			direction.y = temp.x;
+        }
+        void turnRight() {
+			Point temp = direction;
+			direction.x = temp.y;
+			direction.y = -temp.x;
+        }
+        void moveBackward() {
+            point.x = point.x + direction.x;
+            point.y = point.y + direction.y;
+        }
+    };
 
-    bus.publishMoveForward();
 
-    EXPECT_EQ(move_forward_count, 1);
-}
 
-TEST(MotorController, AvoidObstacleWhenLeftBlockedPublishesMoveForward) {
-    EventBus bus;
-    MotorController motor(&bus);
-    int move_forward_count = 0;
-    bus.subScribeMoveForward([&move_forward_count] { ++move_forward_count; });
+    class MotorControllerPowerTest : public ::testing::Test {
+    protected:
+        EventBus bus;
+        FakeMotor motor;
+        std::unique_ptr<MotorController> motorController;
+        FakeSensorProvider sensorProvider;
 
-    FakeSensorProvider fake;
-    fake.left = false;
-    fake.right = true;
-    motor.AvoidObstacle(fake);
+        // 각 TEST_F 실행 직전에 무조건 실행
+        void SetUp() override {
+            motorController = std::make_unique<MotorController>(&bus, motor);
 
-    EXPECT_EQ(move_forward_count, 1);
-}
+            // 앞으로 가도록 유도
+			sensorProvider.left = false;
+            sensorProvider.right = false;
+        }
+    };
 
-TEST(MotorController, AvoidObstacleWhenRightBlockedPublishesMoveForward) {
-    EventBus bus;
-    MotorController motor(&bus);
-    int move_forward_count = 0;
-    bus.subScribeMoveForward([&move_forward_count] { ++move_forward_count; });
+    TEST_F(MotorControllerPowerTest, TurnOn) {
+		motorController->turnOn();
 
-    FakeSensorProvider fake;
-    fake.left = true;
-    fake.right = false;
-    motor.AvoidObstacle(fake);
+        motorController->AvoidObstacle(sensorProvider);
+        
+        // turnOff이므로 Stop 상태 유지
+        ASSERT_TRUE(motor.point.isEqual(Point(1, 0)));
 
-    EXPECT_EQ(move_forward_count, 1);
-}
+        ASSERT_FALSE(motor.direction.isEqual(Point(0, 0)));
+    };
 
-TEST(MotorController, AvoidObstacleWhenBothSidesBlockedStillPublishesMoveForward) {
-    EventBus bus;
-    MotorController motor(&bus);
-    int move_forward_count = 0;
-    bus.subScribeMoveForward([&move_forward_count] { ++move_forward_count; });
+    TEST_F(MotorControllerPowerTest, TurnOff) {
+        motorController->turnOff();
 
-    FakeSensorProvider fake;
-    fake.left = true;
-    fake.right = true;
-    motor.AvoidObstacle(fake);
+        motorController->AvoidObstacle(sensorProvider);
 
-    EXPECT_EQ(move_forward_count, 1);
-}
+		// turnOff이므로 Stop 상태 유지
+        ASSERT_TRUE(motor.point.isEqual(Point(0,0)));
 
-TEST(MotorController, AvoidObstacleViaEventBusUsesSensorControllerState) {
-    EventBus bus;
-    MotorController motor(&bus);
-    int move_forward_count = 0;
-    bus.subScribeMoveForward([&move_forward_count] { ++move_forward_count; });
+        ASSERT_FALSE(motor.direction.isEqual(Point(1, 0)));
+    };
 
-    SensorController sensor(&bus);
-    bus.publishAvoidObstacle(&sensor);
+    class MotorControllerAvoidTest : public ::testing::Test {
+    protected:
+        EventBus bus;
+        FakeMotor motor;
+        std::unique_ptr<MotorController> motorController;
+        FakeSensorProvider sensorProvider;
 
-    EXPECT_EQ(move_forward_count, 1);
+        // 각 TEST_F 실행 직전에 무조건 실행
+        void SetUp() override {
+            motorController = std::make_unique<MotorController>(&bus, motor);
+
+             //전원 켠 상태
+            motorController->turnOn();
+        }
+    };
+
+    TEST_F(MotorControllerAvoidTest, FrontObstacle) {
+        // 앞만 막혀있는 상황
+		sensorProvider.left = false;
+		sensorProvider.right = false;
+
+        motorController->AvoidObstacle(sensorProvider);
+
+        ASSERT_TRUE(motor.point.isEqual(Point(1, 0)));
+        ASSERT_FALSE(motor.point.isEqual(Point(0, 1)));
+        ASSERT_FALSE(motor.point.isEqual(Point(-1, 0)));        
+        ASSERT_FALSE(motor.point.isEqual(Point(0, -1)));
+    };
+
+    TEST_F(MotorControllerAvoidTest, RightObstacle) {
+        // 앞, 오른쪽이 막혀있는 상황
+        sensorProvider.left = false;
+        sensorProvider.right = true;
+
+        motorController->AvoidObstacle(sensorProvider);
+
+        ASSERT_TRUE(motor.point.isEqual(Point(-1, 0)));
+        ASSERT_FALSE(motor.point.isEqual(Point(0, 1)));
+        ASSERT_FALSE(motor.point.isEqual(Point(1, 0)));
+        ASSERT_FALSE(motor.point.isEqual(Point(0, -1)));
+    };
+
+    TEST_F(MotorControllerAvoidTest, LeftObstacle) {
+        // 앞, 왼쪽이 막혀있는 상황
+        sensorProvider.left = true;
+        sensorProvider.right = false;
+
+        motorController->AvoidObstacle(sensorProvider);
+
+        ASSERT_TRUE(motor.point.isEqual(Point(1, 0)));
+        ASSERT_FALSE(motor.point.isEqual(Point(0, 1)));
+        ASSERT_FALSE(motor.point.isEqual(Point(-1, 0)));
+        ASSERT_FALSE(motor.point.isEqual(Point(0, -1)));
+    };
+
 }
